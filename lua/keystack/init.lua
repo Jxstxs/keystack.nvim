398ad080b298c8bd25bbc2e79ec4f27c9983924b
Julius <juleb0202@gmail.com>
2022-09-06 22:57:50 +0200

    Update init.lua

--- lua/keystack/init.lua
+++ lua/keystack/init.lua
@@ -1,6 +1,7 @@
 local M = {
 	-- Functions
 	setup = require("keystack.config").setup,
+	config = require("keystack.config").setup, -- deprecated
 	push = require("keystack.functions").pushMapToStack,
 	pop = require("keystack.functions").popMapFromStack,
 
